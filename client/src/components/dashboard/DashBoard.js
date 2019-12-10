2ae9481d2d7073e9c3acf0267f7024fabda26f87
mohitgarg <mohitgarg083@gmail.com>
2019-12-10 19:25:56 +0530

    45

--- client/src/components/dashboard/DashBoard.js
+++ client/src/components/dashboard/DashBoard.js
@@ -1,13 +1,18 @@
-import React from "react"
+import React,{useEffect} from "react"
 import {connect} from "react-redux"
 import {getCurrentprofile} from "../../actions/profile"
 
-const Dashboard =({profile,getCurrentprofile})=>(
+const Dashboard =({profile,getCurrentprofile,auth})=>{
+    useEffect(()=>{
+        getCurrentprofile()
+    },[])
+    return (
     <div>
         DashBoard
     </div>
-)
+)}
 const mapStatetoProps=state=>({
-    profile:state.profile
+    profile:state.profile,
+    auth:state.auth
 })
 export default connect(mapStatetoProps,{getCurrentprofile})(Dashboard)
