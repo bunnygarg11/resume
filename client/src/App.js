c0d19e5253530b6c79db518508fe525ba378c94e
bunnygarg11 <mohitgarg083@gmail.com>
2019-12-12 21:27:11 +0530

    50

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import Login from "./components/Auth/Login"
 import Dashboard from "./components/dashboard/DashBoard"
 import CreateProfile from "./components/profile-forms/CreateProfile"
 import EditProfile from "./components/profile-forms/EditProfile"
+import AddExperience from "./components/profile-forms/AddExpeience"
+import AddEducation from "./components/profile-forms/AddEducation"
 import Register from "./components/Auth/Register"
 import setAuthToken from "./utils/setAuthToken"
 import {loaduser} from "./actions/auth"
@@ -37,6 +39,8 @@ const App=()=>{
           <PrivateRoute exact path="/dashboard" component={Dashboard}/>
           <PrivateRoute exact path="/create-profile" component={CreateProfile} />
           <PrivateRoute exact path="/edit-profile" component={EditProfile} />
+          <PrivateRoute exact path="/add-experience" component={AddExperience} />
+          <PrivateRoute exact path="/add-education" component={AddEducation} />
         </Switch>
 
       </section>
--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,6 +1,6 @@
 import Axios from "axios"
 import {setAlert} from "./alert"
-import {GET_PROFILE,PROFILE_ERROR} from "./types"
+import {GET_PROFILE,PROFILE_ERROR,UPDATE_PROFILE} from "./types"
 export const getCurrentprofile=()=>async dispatch=>{
     try {
         const res= await Axios.get("/api/profile/me")
@@ -45,4 +45,57 @@ export const createProfile=(formData,history,edit=false)=>async dispatch=>{
           });
         
     }
+}
+export const addExperience=(formData,history)=>async dispatch=>{
+    try {
+        const config={
+            headers:{
+                "Content-Type":"application/json"
+            }
+        }
+        const res=await Axios.put("/api/profile/experience",formData,config)
+        dispatch({
+            type:UPDATE_PROFILE,
+            payload:res.data
+        })
+        dispatch(setAlert("Experience Added","success"))
+        history.push("/dashboard")
+    } catch (err) {
+        const error=err.response.data.errors
+        error.forEach(e=>dispatch(setAlert(e.msg,"danger")))
+        dispatch({
+            type:PROFILE_ERROR,
+            payload:{msg:err.response.statusText,status:err.response.status}
+        })
+        
+    }
+}
+export const addEducation=(formData,history)=>async dispatch=>{
+    try {
+        const config={
+            headers:{
+                "Content-Type":"application/json"
+            }
+
+        }
+
+        const res=await Axios.put("/api/profile/education",formData,config)
+        dispatch({
+            type:UPDATE_PROFILE,
+            payload:res.data
+        })
+        history.push("/dashboard")
+        
+    } catch (err) {
+        const error=err.response.data.errors
+        error.forEach(er=>{
+            dispatch(setAlert(er.msg,"danger"))
+        })
+        dispatch({
+            type:PROFILE_ERROR,
+            payload:{msg:err.statusText,status:err.status}
+        })
+        
+    }
+
 }
